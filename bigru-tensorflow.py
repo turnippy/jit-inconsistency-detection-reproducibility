dbb4125bcb45efbe10399b20a4fedb4f5dee719f
Tong Liu <9tl14@caslab.queensu.ca>
2021-10-21 22:25:04 -0400

    initial commit

---
+++ bigru-tensorflow.py
@@ -0,0 +1,10 @@
+# BiGRU implementation in Tensorflow
+# Contributors: DJ Jin, T Liu, J Yasmin
+# CISC867 Reproducibility study 2021
+# Queen's University, Canada
+
+# The following is an implementation of a bidirectional GRU network.
+# This implementation is based on the PyTorch equivalent implemented by
+# S. Panthaplackel, J.J. Li, G. Milos, and R.J. Mooney (2020),
+# hosted at https://github.com/panthap2/deep-jit-inconsistency-detection
+
