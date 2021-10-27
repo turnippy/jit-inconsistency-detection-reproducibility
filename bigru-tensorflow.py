44b9db48bcb08edafe1d5797bc6c06d32ddb9c58
Tong Liu <9tl14@caslab.queensu.ca>
2021-10-27 12:18:46 -0400

    updated implementation of bigru

--- bigru-tensorflow.py
+++ bigru-tensorflow.py
@@ -8,3 +8,21 @@
 # S. Panthaplackel, J.J. Li, G. Milos, and R.J. Mooney (2020),
 # hosted at https://github.com/panthap2/deep-jit-inconsistency-detection
 
+import tensorflow as tf
+from tensorflow.keras.layers import GRU
+from tensorflow.keras.layers import Bidirectional
+from tensorflow.keras.layers import Dense
+
+class BiGru (tf.Module):
+    def __init__(self, embedding_size, hidden_size,
+                 num_layers, dropout, bidirectional=True):
+        super(BiGru, self).__init__()
+        self.__rnn = Bidirectional(GRU(input_size=embedding_size,
+                         hidden_size=hidden_size,
+                         dropout=dropout,
+                         num_layer=num_layers,
+                         time_major=False)) #data on tensorflow is batch-major by default
+
+    def forward(selfself, src_embedded_tokens, src_lengths, device):
+        encoder_hidden_states, _ = self.__rnn.forward(src_embedded_tokens)
+        return encoder_hidden_states, encoder_final_state
