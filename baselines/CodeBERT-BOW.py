60feaafa23f7459d43523147e93b9632cc7b7683
LofOWL <jeffersonjjin@gmail.com>
2021-10-30 21:27:38 -0400

    checkpoint

--- baselines/CodeBERT-BOW.py
+++ baselines/CodeBERT-BOW.py
@@ -3,7 +3,12 @@ from transformers import RobertaTokenizer, RobertaModel
 from transformers.file_utils import TRANSFORMERS_CACHE
 import torch
 from torch import nn
+import torch.nn.functional as F
+from tqdm import tqdm
+import numpy as np
+import random
 
+from data import ParamDataSet
 
 # ToDo: determine DROPOUT_RATE
 DROPOUT_RATE = 0.5
@@ -12,70 +17,133 @@ CLASSIFICATION_HIDDEN_SIZE = 10
 # ToDo: determine output_size
 output_size = 3
 # ToDo: determine NUM_CLASSES
-NUM_CLASSES = 4
+NUM_CLASSES = 1
 
-# ToDo: determine comment_sequence
-comment_sequence = ""
-# ToDo: determine code_sequence
-code_sequence = ""
-# ToDo: determine labels
-labels = ""
-# ToDo: determine factors
-factor = None
+# # ToDo: determine factors
+# factor = None
+
+class Network(nn.Module):
+
+    def __init__(self,input_size):
+        super(Network, self).__init__()
+
+        self.fc1 = nn.Linear(input_size,CLASSIFICATION_HIDDEN_SIZE)
+        self.fc2 = nn.Linear(CLASSIFICATION_HIDDEN_SIZE,CLASSIFICATION_HIDDEN_SIZE)
+        # self.classification_dropout_layer = nn.Dropout(p=DROPOUT_RATE)
+        self.output_layer = nn.Linear(CLASSIFICATION_HIDDEN_SIZE,NUM_CLASSES)
+
+    def forward(self,input):
+        output = self.fc1(input)
+        output = F.relu(output)
+        output = self.fc2(output)
+        # output = self.classification_dropout_layer(output)
+        output = self.output_layer(output)
+        # output = F.softmax(output,dim=-1)
+        output = F.sigmoid(output)
+        return output
+
+    def train(self,dataset):
+        iter = 0
+        for data,labels in dataset:
+
+            # Clear gradients w.r.t. parameters
+            optimizer.zero_grad()
+
+            # Forward pass to get output/logits
+            outputs = model(data)
+
+            # Calculate Loss: softmax --> cross entropy loss
+            loss = F.nll_loss(outputs, labels)
+
+            # Getting gradients w.r.t. parameters
+            loss.backward()
+
+            # Updating parameters
+            optimizer.step()
+
+            print(f'{iter} {loss.item()}')
+        print(f'{iter} {loss.item()}')
 
 
-def get_inputs(input_text):
+class CodeBERT:
 
-    tokens = tokenizer.tokenize(input_text)
+    def __init__(self):
+        self.model = RobertaModel.from_pretrained(
+        "microsoft/codebert-base", cache_dir=TRANSFORMERS_CACHE)
+        self.tokenizer = RobertaTokenizer.from_pretrained(
+        "microsoft/codebert-base", cache_dir=TRANSFORMERS_CACHE)
+        self.max_length = 512
 
-    length = min(len(tokens), max_length)
+    def get_inputs(self,input_text):
+        tokens = self.tokenizer.tokenize(input_text)
+        length = min(len(tokens), self.max_length)
+        tokens = tokens[:length]
+        token_ids = self.tokenizer.convert_tokens_to_ids(tokens)
+        padding_length = self.max_length - len(tokens)
+        token_ids += [self.tokenizer.pad_token_id]*padding_length
+        return token_ids, length
 
-    tokens = tokens[:length]
-    token_ids = tokenizer.convert_tokens_to_ids(tokens)
-    padding_length = max_length - len(tokens)
-    token_ids += [tokenizer.pad_token_id]*padding_length
-    return token_ids, length
+    def get_bow_representation(self,sequence):
+        input_ids, length = self.get_inputs(sequence)
+        mask = (torch.arange(self.max_length) < length).unsqueeze(-1).float()
 
+        # embeddings = model.embeddings(input_ids) * mask
+        embeddings = self.model.embeddings(torch.tensor(input_ids).unsqueeze(0)) * mask
+        # factor ?
+        # vector = torch.sum(embeddings, dim=1)/torch.sum(factor, dim=1)
+        # return vector
+        return torch.sum(embeddings, dim=1)
 
-def get_bow_representation(sequence):
+class DataSet:
 
-    input_ids, length = get_inputs(sequence, max_length)
-    mask = (torch.arange(max_length) < length).unsqueeze(-1).float()
-    embeddings = model.embeddings(input_ids) * mask
-    # factor ?
-    vector = torch.sum(embeddings, dim=1)/torch.sum(factor, dim=1)
-    return vector
+    def __init__(self):
+        self.cb = CodeBERT()
+        self.max_length = self.cb.max_length
 
+    def get_raw_data(self,dataset):
+        return ParamDataSet(dataset,
+        "old_comment_raw", "old_code_raw", "label")
+
+    def get_feature_vector(self,comment_sequence,code_sequence):
+        comment_vector = self.cb.get_bow_representation(comment_sequence)
+        code_vector = self.cb.get_bow_representation(code_sequence)
+        feature_vector = torch.cat([comment_vector, code_vector], dim=-1)
+        return feature_vector
+
+    def get_data_set(self,dataset):
+        comment_sequences, code_sequences, labels = self.get_raw_data(dataset)
+        print(len(comment_sequences))
+        test_set = list()
+        for comment_sequence,code_sequence,label in tqdm(zip(comment_sequences,code_sequences,labels)):
+            feature_vector = self.get_feature_vector(comment_sequence,code_sequence)
+            test_set.append([feature_vector,torch.tensor([label])])
+    
+        return test_set
 
 if __name__ == "__main__":
-    print("CodeBERT-BOW baseline")
+    print(f"CodeBERT-BOW Baseline")
 
-    tokenizer = RobertaTokenizer.from_pretrained(
-        "microsoft/codebert-base", cache_dir=TRANSFORMERS_CACHE)
-    model = RobertaModel.from_pretrained(
-        "microsoft/codebert-base", cache_dir=TRANSFORMERS_CACHE)
+    ds = DataSet()
 
-    max_length = 512
+    # train data set require memory more than 32 GB
+    data_set = ds.get_data_set('test')
+    train,test = data_set[:int(len(data_set)*0.8)],data_set[int(len(data_set)*0.8):]
+    print(f'{len(train)} {len(test)}')
 
-    print(tokenizer)
+    # forward network
+    input_size = 1536
+    model = Network(input_size)
+    learning_rate = 0.1
+    optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)  
 
-    print(model)
+    # train the model
+    # enable to run, but not sure
+    model.train(train)
 
-    classification_dropout_layer = nn.Dropout(p=DROPOUT_RATE)
+    test_output = model(test[0][0])
 
-    fc1 = nn.Linear(output_size, CLASSIFICATION_HIDDEN_SIZE)
-    fc2 = nn.Linear(CLASSIFICATION_HIDDEN_SIZE, CLASSIFICATION_HIDDEN_SIZE)
-    output_layer = nn.Linear(CLASSIFICATION_HIDDEN_SIZE, NUM_CLASSES)
+    print(test_output)
+    print(test[0])
 
-    comment_vector = get_bow_representation(comment_sequence)
 
-    code_vector = get_bow_representation(code_sequence)
-
-    feature_vector = torch.cat([comment_vector, code_vector], dim=-1)
-
-    logits = output_layer(classification_dropout_layer(
-        torch.nn.functional.relu(fc1(feature_vector))))
-
-    logprobs = torch.nn.functional.log_softmax(logits, dim=-1)
-
-    loss = torch.nn.functional.nll_loss(logprobs, labels)
+    
---
+++ baselines/data.py
@@ -0,0 +1,27 @@
+import os
+import json
+
+path = "./data"
+data_elements = os.listdir(path)
+param_path = f'{path}/{data_elements[0]}'
+
+def ParamDataSet(dataset,*kwgs) -> list:
+    
+    param_test = f'{param_path}/{dataset}.json'
+    with open(param_test,"r") as files: 
+        param_test = json.loads(files.read())
+    result = {kwg:[] for kwg in kwgs}
+    for param in param_test:
+        for kwg in kwgs:
+            result[kwg] = result[kwg] + [param[kwg]]
+    return result.values()
+
+if __name__ == "__main__":
+    data_elements = os.listdir(path)
+    
+    param_path = f'{path}/{data_elements[0]}'
+    result = ParamDataSet('train',"old_comment_raw","old_code_raw")
+    
+    train = ParamDataSet('test',"old_comment_raw","old_code_raw")
+    
+    
